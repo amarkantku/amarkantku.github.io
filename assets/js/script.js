e9cac6e25fa681dcb9e04335b96b5cbbdb87f7fe
Amarkant Kumar <amar.du2013@gmail.com>
2019-12-17 21:40:14 +0530

    address updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,6 @@
 $('.navbar').on('click', 'a[href^="#"]', function (event) {
-	console.log(event);
     event.preventDefault();
-    let top = 0;
-    if(event.target.hash === '#skills') {
-    	top = -20;
-    }
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top + top
+        scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
