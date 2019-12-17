ab253d91445fb906ec76585424bc3bf495a17508
Amarkant Kumar <amar.du2013@gmail.com>
2019-12-17 21:29:45 +0530

    address updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $('.navbar').on('click', 'a[href^="#"]', function (event) {
+	console.log(event);
     event.preventDefault();
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
